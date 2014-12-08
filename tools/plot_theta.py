e4e72cb0233f3df26313a34990f83d6a984e2c9f
aicm <aicm1f12@soton.ac.uk>
2014-12-08 20:20:38 +0000

    Bug corrected in plot_theta.py (also done a few hours ago on the main branch)

--- tools/plot_theta.py
+++ tools/plot_theta.py
@@ -93,7 +93,7 @@ def find_solute_theta(molecule,restart='restart',results='results') :
     elif mol_type is "gcsolute" :
       res_thetavals = snap.thetavals
     for ind,res_theta in enumerate(res_thetavals) :
-      thetas[ind+1].append(res_theta)
+      if ind+1 in thetas.keys() : thetas[ind+1].append(res_theta)
   return thetas
 
 
@@ -121,23 +121,73 @@ def plot_dist(dict_th,plotname="theta_dist",xlab="theta") :
     the plots will be saved in png files
   """
 
+  def set_axes(xname,yname) :
+    plt.xlabel(xname,fontsize=20)
+    plt.ylabel(yname,fontsize=15)
+
   if xlab == "theta" : xlab = r"$\theta$"
-  def set_axes() :
-    plt.xlabel(xlab,fontsize=20)
-    plt.ylabel("Frequency",fontsize=15)
 
   thetas_values = np.array(dict_th.values(),float)
 
   n, bins, patches = plt.hist(np.reshape(thetas_values,newshape=-1),bins=100,facecolor=simulationobjects.color(0),histtype='stepfilled')
-  set_axes()
+  set_axes(xlab,"Frecuency")
   plt.savefig(plotname+"_all.png")
   plt.clf()
 
   n, bins, patches = plt.hist(np.transpose(thetas_values),bins=100,histtype="step")
-  set_axes()
+  set_axes(xlab,"Frecuency")
   plt.savefig(plotname+"_each.png")
+  plt.clf()
+
+  for i in dict_th :
+    plt.plot(dict_th[i])
+  set_axes(r"$\theta$","Snapshot")
+  plt.savefig(plotname+"_snapshot.png")
   
+    
+
+def extract_theta_pdb (thetas_dic, theta_range, pdbfile, outpdb, residue) :
+  """
+  Extract the molecules which theta is
+  within a given range
   
+  Parameters
+  ----------
+  thetas_dic : dict
+    the theta values per solute id
+    in snapshot order 
+  theta_range : list
+    the lower and upper limit of the
+    theta range
+  pdbfile : string
+    the name of the pdb file product
+    of the ProtoMS simulation
+  outpdb : string
+    the name of the pdbfile where
+    the extracted molecules will be
+    printed
+
+  Return
+  ------
+  PDBFile :
+    the PDBFile object with the
+    molecules within the given theta range
+  """
+
+  try :
+    theta_range = [float(limit) for limit in theta_range]
+  except :
+    raise simulationobjects.SetupError("The limits of the theta range could not be undestood")
+
+  pdbin_obj = simulationobjects.PDBSet()
+  pdbin_obj.read(filename=pdbfile, resname=residue)
+  
+  for sol_id  in thetas_dic :
+      for snapshot, each_theta in enumerate([float(theta) for theta in thetas_dic[sol_id]]) :
+        if theta_range[0] <= each_theta <= theta_range[1] :
+          print each_theta
+          print sol_id
+          print pdbin_obj.pdbs[snapshot].residues[sol_id]
 
 #
 # If this is run from the command-line
@@ -152,8 +202,23 @@ if __name__ == '__main__' :
   parser.add_argument('-s','--restart',help="the replica values to plot. Default='restart'",default='restart')
   parser.add_argument('-m','--molecule',help="the residue name of the JAWS molecule. Default='WAT'",default="WAT")
   parser.add_argument('-p','--plotname',help="the start of the filename for the plots generated. Default='theta_dist'",default="theta_dist")
+  parser.add_argument('-tr','--thetarange',nargs=2,help="the range of thetas which correponding molecules will be extracted, specified by a lower and an upper limit. By default, no molecules will be extracted.")
+  parser.add_argument('-pdb','--pdbfile',help="the pdb file result of protoms. Only required if when molecules are extracted for a range of thetas. Default = 'all.pdb'",default='all.pdb')
+  parser.add_argument('-op','--outpdb',help="the name of the file where the molecules for a given range of thetas are to be extracted. Default = 'out_thetas.pdb'",default='out_thetas.pdb')
   args = parser.parse_args()
 
   thetas_dic = find_solute_theta(args.molecule,args.restart,args.results)
 
   plot_dist(thetas_dic,plotname=args.plotname)
+
+  if args.thetarange :
+    extract_theta_pdb(thetas_dic,args.thetarange,args.pdbfile,args.outpdb,args.molecule)
+
+
+
+
+
+
+
+
+
