3791d97cde01b43960ff01960c0a44f7bbec417b
devasena_soton <devasena_soton@localhost>
2015-07-24 17:10:33 +0100

    Included tests for AmberTools dependencies.

--- test-suite/test_overall/test_dependencies/test_install_dependencies.py
+++ test-suite/test_overall/test_dependencies/test_install_dependencies.py
@@ -14,6 +14,9 @@ import sys
 import nose
 import nose.tools as nt
 
+amber_env = os.environ["AMBERHOME"]
+amber_path = amber_env + "/AmberTools/bin"
+
 class TestDependInstallation(unittest.TestCase):
 
 #Check that Python dependencies are installed correctly.
@@ -36,14 +39,28 @@ class TestDependInstallation(unittest.TestCase):
          except ImportError as e:
              print e
 
+#Checking for AmberTools installation dependencies
+
      def test_ambertools_dependencies(self):
       
          try:
              self.assertTrue(os.getenv("AMBERHOME"))
          except KeyError as e:
              print e
+    	     print("AMBERHOME is not set.")
 
-         
+         try:
+             self.assertTrue(os.path.exists(amber_path + "/antechamber"))
+	 except IOError as e:
+  	     print e
+  	     print("antechamber AmberTools module doesn't exist.")
+    
+	 try:
+             self.assertTrue(os.path.exists(amber_path + "/parmchk"))
+	 except IOError as e:
+  	     print e
+  	     print("parmchk AmberTools module doesn't exist.")
+
 
 if  __name__ == '__main__':
     unittest.main()
