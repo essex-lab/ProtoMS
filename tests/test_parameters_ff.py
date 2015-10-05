1a62df4f64e9f2f68479ff2f114670ebe251d168
devasena_soton <devasena_soton@localhost>
2015-10-05 09:47:43 +0100

    Added test for protoms ff files, parameters and template check

---
+++ tests/test_parameters_ff.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python2.7
+
+"""
+Tests for ProtoMS python module (protoms.py): Setup tests
+"""
+
+#---------------------------------------------
+# Imports
+#---------------------------------------------
+
+import os
+import unittest
+import sys
+import nose
+import nose.tools as nt
+import re
+
+#---------------------------------------------
+# ProtoMS parameters test
+#---------------------------------------------
+
+# Storing PROTOMSHOME environment variable to a python variable.
+proto_env = os.environ["PROTOMSHOME"]
+ff_tem_files = ["amber99-residues-mod.ff", "amber99-residues.ff", "amber99.ff", "amber99SB.ff", "gaff.ff", "gaff.types", "gaff14.ff", "gaff14.types", "gborn.parameters", "gbornrevised.parameters","gold-residues.ff", "opls96-residues.ff", "opls96.ff", "rotamlib", "solvents.ff", "surface.parameters"]
+
+class TestToolsSetUp(unittest.TestCase):
+
+# Test if ProtoMS tools and reference files exist are in the expected place.
+    def setUp(self):
+        super(TestToolsSetUp, self).setUp()
+        print("Setting up ProtoMS tools test.")
+    
+    def tearDown(self):
+        super(TestToolsSetUp, self).setUp()
+        print("Cleaning up ProtoMS tools test.")
+
+    def test_tools(self):
+        
+        for tem_files in ff_tem_files:
+
+            try:
+                self.assertTrue(os.path.exists(proto_env + "/parameter/" + tem_files))
+    
+            except IOError as e:
+                print e
+	        print("Reference files/ force field parameter file ", tem_files, "is not present in the expected place.")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
