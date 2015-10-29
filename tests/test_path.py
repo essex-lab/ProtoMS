52f834a33aa8a08faac27085a6d7960a47385191
devasena_soton <devasena_soton@localhost>
2015-10-29 12:09:25 +0000

    Updated PROTOMSHOME test

--- tests/test_path.py
+++ tests/test_path.py
@@ -15,44 +15,26 @@ import nose
 import nose.tools as nt
 import subprocess
 import re
-#---------------------------------------------
-# Setup tests
-#---------------------------------------------
 
-# Storing PROTOMSHOME environment variable to a python variable.
-proto_env = os.environ["PROTOMSHOME"]
-proto_path = os.popen("pwd").read()
-proto_path = re.sub('\\n$', '', proto_path)
-
+#---------------------------------------------
+# PROTOMSHOME test
+#---------------------------------------------
 
 class TestSetUp(unittest.TestCase):
 
 # Test if PROTOMSHOME is set properly. PROTOMSHOME must be set after protoms install at HOME directory.
     def setUp(self):
         super(TestSetUp, self).setUp()
-        print("Setting up PROTOMSHOME check test case.") 
+        print("Setting up PROTOMSHOME check test.")
 
     def tearDown(self):
         super(TestSetUp, self).tearDown()
-        print("Cleaning up PROTOMSHOME check test case.")
+        print("Cleaning up PROTOMSHOME check test.")
 
     def test_protoms_path(self):
         
-        try:
-            os.path.exists(proto_path)
-            os.getenv("PROTOMSHOME")
+        self.assertIsNotNone(os.getenv("PROTOMSHOME"))
         
-        except KeyError, e:
-            print e
-            print("PROTOMSHOME is not set.")
-
-     
-        try:
-            self.assertEqual(proto_env,proto_path,msg=None)
-
-        except:
-            print("PROTOMSHOME is not set properly.")
-
 
 if  __name__ == '__main__':
     unittest.main()
