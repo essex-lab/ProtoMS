d013390f4b90e4c2d39177bf5c4c2d0e855d475e
devasena_soton <devasena_soton@localhost>
2015-11-19 15:16:31 +0000

    Updated content check of output files in the RETI dual topology test

--- tests/test_RETI_dbl/test_reti_dbl.py
+++ tests/test_RETI_dbl/test_reti_dbl.py
@@ -26,8 +26,10 @@ from subprocess import call
 
 proto_env = os.environ["PROTOMSHOME"]
 test_dir = proto_env + "/tests/test_RETI_dbl/"
+ref_dir = proto_env + "/tests/RETI_dbl/"
 output_files_setup = ["ethane_box.pdb", "eth-meo.tem", "run_free.cmd"]
 out_sim_files = ["results", "accept", "all.pdb", "restart.prev", "warning", "info", "restart", "results_inst"]
+outfiles = ["ethane_box.pdb", "eth-meo.tem", "run_free.cmd"]
 
 class TestRETIdbl(unittest.TestCase):
     """Test for RETI/ MPI function - short dual topology simulation."""
@@ -48,9 +50,17 @@ class TestRETIdbl(unittest.TestCase):
             for out_files in output_files_setup:
 	        self.assertTrue(os.path.exists(test_dir + out_files), "ProtoMS setup output file %s is missing. There could be problems with ligand's zmat generation, forcefield issues, template generation issues and ProtoMS input command file generation for simulation." % (test_dir + out_files))
 
+            """Checking content of RETI dual topology setup output files with reference data files."""  
+            for out_files in outfiles:
+                if((call("diff "+ test_dir + out_files + " "+ ref_dir + out_files, shell=True)) == 0):
+                    continue
+                else:
+                    raise ValueError("Content mismatch between output and reference %s" %(out_files))  
 
 	else:
             raise simulationobjects.SetupError("ProtoMS RETI dual topology setup and command files generation failed")
+
+        """Test for RETI dual topology simulation."""
 
         if((call("mpirun -np 4 $PROTOMSHOME/protoms3 run_free.cmd", shell=True)) == 0):
 
@@ -60,7 +70,20 @@ class TestRETIdbl(unittest.TestCase):
                     if len(dirs) != 0:
                         for d in dirs:
                             for out_files in out_sim_files:
-                                self.assertTrue(os.path.exists(os.path.join("out_free",d,out_files)), "Simulation file %s is missing. Please check!" % (os.path.join("out_free",d,out_files)))
+                                self.assertTrue(os.path.exists(os.path.join("out_free/",d+"/",out_files)), "Simulation file %s is missing. Please check!" % (os.path.join("out_free",d+"/",out_files)))
+
+                                """Checking content of RETI free phase leg of a dual topology simulation output files with reference data. """
+                                if out_files == "info":
+                                    if((call("bash "+test_dir+"content_info_comp.sh", shell=True)) == 0):
+                                        continue
+                                    else:
+                                        raise ValueError("Content mismatch between output and reference info file for lambda value %s."%(d))
+                                else:
+                                    if((call("diff "+ "out_free/" + d + "/"+ out_files+ " " + ref_dir +"out_free/" + d + "/"+out_files,shell=True)) == 0):
+                                        continue
+                                    else:
+                                        raise ValueError("Content mismatch between output and reference %s." %(os.path.join(test_dir,"out_free/",d+"/",out_files)))
+                            
             
         else:
             raise simulationobjects.SetupError("RETI dual topology simulation is not successful.")
