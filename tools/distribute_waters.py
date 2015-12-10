c675500943b70c7beaf557a80ca542e94f4a46b2
James Graham <J.A.Graham@soton.ac.uk>
2015-12-10 10:35:38 +0000

    Fixed setup random seed in protoms.py;

--- tools/distribute_waters.py
+++ tools/distribute_waters.py
@@ -75,7 +75,7 @@ def distribute_particles(box, particles, watermodel="t4p",resname="WA1",partnumb
   Parameters
   ----------
   box : a list or dictionary
-    the origin and lenght of the box
+    the origin and length of the box
     where the molecules will be placed
   particles : string or PDB object
     the number of waters to include in the box
@@ -114,8 +114,8 @@ def distribute_particles(box, particles, watermodel="t4p",resname="WA1",partnumb
     logger.debug("\tparticles %s"%particles.name)
   logger.debug("\twatermodel %s"%watermodel)
   logger.debug("\tpartnumb %s"%partnumb)
-  if ranseed is not None:
-      logger.debug("\transeed %d"%ranseed)
+  if ranseed is not None :
+    logger.debug("\transeed %d"%ranseed)
   logger.debug("This will distribute the molecules given in 'particles' randomly in the box given in box. If a number is given in 'particles', it assumes them to be waters.")
 
   np.random.seed(ranseed)
@@ -187,12 +187,12 @@ if __name__ == "__main__":
   parser.add_argument('--model', help="Water model. Used when only the amount of waters is specified. Options: 't4p','t3p'. Default='t4p'",default='t4p')
   parser.add_argument('--resname',help="Residue name of the molecules writen to output. Default='WAT'",default='WAT')
   parser.add_argument('--number',help="Required number of molecules when it differs from the number of residues in the file.",default=None)
-  parser.add_argument('--ranseed',help="Optional random number seed for generation of water coordinates.",default=None)
+  parser.add_argument('--setupseed',help="Optional random number seed for generation of water coordinates.",default=None,type=int)
   args = parser.parse_args()
 
   logger = simulationobjects.setup_logger("distribute_waters_py.log")
 
-  outobj = distribute_particles(args.box,args.molecules,args.model,args.resname,args.number,args.ranseed)
+  outobj = distribute_particles(args.box,args.molecules,args.model,args.resname,args.number,args.setupseed)
   outobj.write(filename=args.outfile)
   print "\nMolecules printed in %s"%args.outfile
 
