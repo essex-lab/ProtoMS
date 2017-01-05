caf514a5254be2653918ca3a7e17417467e40a9e
James Graham <J.A.Graham@soton.ac.uk>
2017-01-05 16:29:29 +0000

    Update and fix reti_dbl test

---
+++ tests/test_reti_dbl.py
@@ -0,0 +1,70 @@
+import unittest
+
+import framework
+
+
+class RetiDblSetupTest(framework.BaseTest):
+    ref_dir = "tests/RETI_dbl/"
+
+    copy_files = [
+        "ethane.pdb",
+        "methanol.pdb"
+    ]
+
+    setup_args = [
+        "-s", "dualtopology",
+        "-l", "ethane.pdb", "methanol.pdb",
+        "--lambdas", "0.00", "0.33", "0.67", "1.00",
+        "--nequil", "0",
+        "--nprod", "10",
+        "--ranseed", "100000",
+        "--dumpfreq", "1",
+        "--cleanup",
+        "--gaff", "gaff14"
+    ]
+
+    setup_output_files = [
+        "ethane_box.pdb",
+        "eth-meo.tem",
+        "run_free.cmd"
+    ]
+
+
+class RetiDblSimulationTest(framework.BaseTest):
+    ref_dir = "tests/RETI_dbl/"
+
+    copy_files = [
+        "run_free.cmd",
+        "eth-meo.tem",
+        "ethane.pdb",
+        "methanol.pdb",
+        "ethane_box.pdb"
+    ]
+
+    simulation_mpi_processes = 4
+
+    simulation_args = [
+        "run_free.cmd"
+    ]
+
+    simulation_output_directories = [
+        "out_free/lam-0.000",
+        "out_free/lam-0.330",
+        "out_free/lam-0.670",
+        "out_free/lam-1.000"
+    ]
+
+    simulation_output_files = [
+        "accept",
+        "all.pdb",
+        "info",
+        "restart",
+        "restart.prev",
+        "results",
+        "results_inst",
+        "warning"
+    ]
+
+
+if __name__ == '__main__':
+    unittest.main()
