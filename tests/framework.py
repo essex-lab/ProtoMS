87a331081906d8b50e6c75de5a1097c853d1ee75
James Graham <J.A.Graham@soton.ac.uk>
2017-01-09 11:46:38 +0000

    Simplify and added attribute docstrings to framework.py

--- tests/framework.py
+++ tests/framework.py
@@ -10,7 +10,43 @@ import filecmp
 import errno
 
 
+class TestDefinitionError(BaseException):
+    pass
+
+
 class BaseTest(unittest.TestCase):
+    # Class attributes used to define a new test
+
+    setup_executable = os.path.join("tools", "protoms.py")
+    """ProtoMS executable to be run during setup phase"""
+    simulation_executable = os.path.join("build", "protoms3")
+    """ProtoMS executable to be run during simulation phase"""
+
+    ref_dir = None
+    """Directory containing the test reference files"""
+    copy_files = None
+    """Files to copy to the test's working directory at the beginning of the test"""
+
+    setup_args = None
+    """List of arguments provided to the executable during the setup phase"""
+    setup_output_files = None
+    """Output files to check against reference at the end of the setup phase"""
+
+    simulation_args = None
+    """List of arguments provided to the executable during the simulation phase"""
+    simulation_output_directory = None
+    """Name of the simulation output directory if there is only one"""
+    simulation_output_directories = None
+    """List of names of the simulation output directories if there is multiple"""
+    simulation_output_files = None
+    """Output files to check against reference at the end of the simulation phase"""
+
+    simulation_mpi_processes = 0
+    """Number of MPI processes to use during the simulation stage - default is no MPI"""
+
+    _do_test_setup = False
+    _do_test_simulation = False
+
     @classmethod
     def setUpClass(cls):
         super(BaseTest, cls).setUpClass()
@@ -31,64 +67,39 @@ class BaseTest(unittest.TestCase):
         try:
             protoms_env = os.environ["PROTOMSHOME"]
         except KeyError:
-            raise Exception("PROTOMSHOME environment variable is not set.")
+            raise EnvironmentError("PROTOMSHOME environment variable is not set.")
 
-        try:
-            cls.setup_executable = os.path.join(protoms_env, "tools", cls.setup_executable)
-        except AttributeError:
-            cls.setup_executable = os.path.join(protoms_env, "protoms.py")
-
-        try:
-            cls.simulation_executable = os.path.join(protoms_env, "build", cls.simulation_executable)
-        except AttributeError:
-            cls.simulation_executable = os.path.join(protoms_env, "build", "protoms3")
+        cls.setup_executable = os.path.join(protoms_env, cls.setup_executable)
+        cls.simulation_executable = os.path.join(protoms_env, cls.simulation_executable)
 
         cls.all_files = []
 
         # These are required for every test
-        try:
-            cls.ref_dir
-            cls.copy_files
+        if cls.ref_dir is None or cls.copy_files is None:
+            raise TestDefinitionError("You must provide ref_dir and copy_files when defining a test case. Check framework.py")
 
-            cls.all_files.extend(cls.copy_files)
-        except AttributeError:
-            raise AttributeError("You must provide ref_dir and copy_files when defining a test case. Check framework.py")
+        cls.all_files.extend(cls.copy_files)
         cls.full_ref_dir = os.path.join(protoms_env, cls.ref_dir)
 
         # For tests of setup functionality these are required
-        try:
-            cls.setup_args
-            cls.setup_output_files
-
-            cls.do_test_setup = True
+        if cls.setup_args is not None and cls.setup_output_files is not None:
+            cls._do_test_setup = True
             cls.all_files.extend(cls.setup_output_files)
-        except AttributeError:
-            cls.do_test_setup = False
 
         # For tests of simulation functionality these are required
-        try:
-            cls.simulation_args
-            cls.simulation_output_files
-
-            try:
-                cls.simulation_mpi_processes
-            except AttributeError:
-                cls.simulation_mpi_processes = 0
-
-            try:
-                if type(cls.simulation_output_directories) is str:
-                    cls.simulation_output_directories = [cls.simulation_output_directories]
-            except AttributeError:
+        if cls.simulation_args is not None and cls.simulation_output_files is not None:
+            if cls.simulation_output_directories is None:
                 cls.simulation_output_directories = [cls.simulation_output_directory]
-            cls.do_test_simulation = True
+            elif type(cls.simulation_output_directories) is str:
+                cls.simulation_output_directories = [cls.simulation_output_directories]
+
+            cls._do_test_simulation = True
 
             output_files = [os.path.join(d, f) for f in cls.simulation_output_files for d in cls.simulation_output_directories]
             cls.all_files.extend(output_files)
-        except AttributeError:
-            cls.do_test_simulation = False
 
-        if not cls.do_test_setup and not cls.do_test_simulation:
-            raise Exception("Test case runs neither setup nor simulation, it is defined incorrectly.")
+        if not cls._do_test_setup and not cls._do_test_simulation:
+            raise TestDefinitionError("Test case runs neither setup nor simulation, it is defined incorrectly.")
 
         # Delete files left over from previous test runs
         cls.helper_clean_files()
@@ -106,10 +117,12 @@ class BaseTest(unittest.TestCase):
                 os.remove(filename)
             except OSError:
                 pass
+
         try:
-            if os.path.isdir(cls.simulation_output_directory) and not os.listdir(cls.simulation_output_directory):
-                os.rmdir(cls.simulation_output_directory)
-        except AttributeError:
+            for dir in cls.simulation_output_directories:
+                if os.path.isdir(dir) and not os.listdir(dir):
+                    os.rmdir(dir)
+        except TypeError:
             pass
 
     def helper_subprocess_call(self, args):
@@ -140,7 +153,7 @@ class BaseTest(unittest.TestCase):
             except IOError:
                 raise IOError("The required reference input file {0} could not be copied".format(filename))
 
-        if self.do_test_setup:
+        if self._do_test_setup:
             print("\nTEST_SETUP_RUN\n")
             args = [self.setup_executable] + self.setup_args
             self.helper_subprocess_call(args)
@@ -148,7 +161,7 @@ class BaseTest(unittest.TestCase):
             print("\nTEST_SETUP_OUTPUT\n")
             self.helper_check_output("setup", self.setup_output_files)
 
-        if self.do_test_simulation:
+        if self._do_test_simulation:
             print("\nTEST_SIMULATION_RUN\n")
             args = [self.simulation_executable] + self.simulation_args
             if self.simulation_mpi_processes > 0:
