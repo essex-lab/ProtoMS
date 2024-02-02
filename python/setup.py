a8103e54395c4440b5d727f23d2f7628ec6064ca
KareemMarzouk <bkmarzouk@outlook.com>
2024-02-02 10:47:20 +0000

    refomat python/setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 
-setup(name='protomslib',
-      description='Library providing the python components of the ProtoMS simulation package',
-      version='3.4',
-      author='Essex Research Group',
-      author_email='jessexgroup@gmail.com',
-      packages=['protomslib', 'protomslib.prepare', 'protomslib.free_energy'],
-      url='http://www.protoms.org')
+setup(
+    name="protomslib",
+    description="Library providing the python components of the ProtoMS simulation package",
+    version="3.4",
+    author="Essex Research Group",
+    author_email="jessexgroup@gmail.com",
+    packages=["protomslib", "protomslib.prepare", "protomslib.free_energy"],
+    url="http://www.protoms.org",
+)
