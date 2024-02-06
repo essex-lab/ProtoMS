7bc76ee28fb1db476f7a42468070717d998390bc
KareemMarzouk <bkmarzouk@outlook.com>
2024-02-06 12:41:29 +0000

    add install_requires into setup.py

--- python/setup.py
+++ python/setup.py
@@ -8,4 +8,11 @@ setup(
     author_email="jessexgroup@gmail.com",
     packages=["protomslib", "protomslib.prepare", "protomslib.free_energy"],
     url="https://www.protoms.org",
+    install_requires=[
+        "numpy>=1.20.0",
+        "scipy>=1.6.3"
+        "matplotlib>=3.4.2",
+        "six>=1.16",
+        "nose>=1.3.7",
+    ]
 )
